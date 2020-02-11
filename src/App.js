59f47b8c27d904cfde21fa0e9316ef581323b8d7
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-02-10 21:03:30 -0800

    Updated to work with env file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SearchBar from "./SearchBar";
 import PhotoCarousel from "./PhotoCarousel";
 
 function App() {
+  console.log(process.env);
   return (
     <div className="App">
       <div className="Header">
