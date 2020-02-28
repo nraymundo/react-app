415e927692bd8a5e660e53a452efad644dd4df9b
Nicolas Raymundo <nicoraymundo2@gmail.com>
2020-02-28 13:19:59 -0800

    Connected api with search bar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,33 +9,45 @@ function getId(query) {
   console.log(query);
 }
 
-function SearchBar({ setCurrDisplay }) {
+function SearchBar() {
   const [error, setError] = useState(null);
   const [query, setQuery] = useState("");
   const [photo, setPhoto] = useState([]);
 
-  const handleQueryChange = event => setQuery(event.target.value);
+  const apiKey =
+    "a920466e5973644c09180f9583b9cb3b409c600d84fd46f025946ca2ebb23f7f";
 
-  const performQuery = async event => {
+  const fetchAPI = event => {
     event.preventDefault();
-
-    setError(null);
-
-    try {
-      const result = await searchTeams({
-        // team: getId(query),
-        category: "search"
+    let api = `https://api.unsplash.com/search/photos/?client_id=${apiKey}&query=${query}&per_page=4`;
+    fetch(api)
+      .then(response => response.json())
+      .then(content => {
+        console.log("content" + content.results[0].urls);
+        setPhoto(content.results);
       });
-      setPhoto(result);
-    } catch (error) {
-      setError("Sorry, that photo doesn't exist.");
-    }
   };
 
+  const handleQueryChange = event => setQuery(event.target.value);
+
+  // const performQuery = async event => {
+  //   event.preventDefault();
+
+  //   setError(null);
+
+  //   try {
+  //     const result = await searchTeams({
+  //       category: "search",
+  //     });
+  //     setPhoto(result);
+  //   } catch (error) {
+  //     setError("Sorry, that photo doesn't exist.");
+  //   }
+  // };
+
   return (
-    <form className="SearchForm" onSubmit={performQuery}>
+    <form className="SearchForm" onSubmit={fetchAPI}>
       <div className="SearchContent">
-        {/* <input type="submit" id="SearchButton" /> */}
         <input
           id="Search"
           name="query"
@@ -45,21 +57,19 @@ function SearchBar({ setCurrDisplay }) {
           onChange={handleQueryChange}
           placeholder={"Search photos"}
         />
-        {/* <input type="submit" id="SearchButton" value="Search" /> */}
-        <button
-          id="SearchButton"
-          type="button"
-          disabled={!query}
-          onClick={() => setCurrDisplay("search")}
-        >
+        <button id="SearchButton" type="button" disabled={!query}>
           Search
         </button>
       </div>
 
       {error && <div className="error">{error}</div>}
-      {console.log(photo)}
 
-      <SearchResults info={photo} />
+      <div>
+        {photo &&
+          photo.map(photos => (
+            <img src={photos.urls.regular} alt={photos.description}></img>
+          ))}
+      </div>
     </form>
   );
 }
