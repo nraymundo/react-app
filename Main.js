a03716f6d1e31b8aa0dba0d8e8dfaef0c1107c5c
meganreyes <45610592+meganreyes@users.noreply.github.com>
2020-03-05 15:32:20 -0800

    Added random mood feature & changed title

---
+++ Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Main.css";
+import SearchBar from "./SearchBar";
+import { usePalette } from "react-palette";
+
+export default function Main() {
+  return (
+    <div className="MainContent">
+      <h1 className="MainText">MoodMi</h1>
+    </div>
+  );
+}
